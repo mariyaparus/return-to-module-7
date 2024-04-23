4fa2bf083d6905f764ca1d5b6ec4ed2929f15e7f
MariyaParus <cat-she@mail.ru>
2024-04-23 11:30:10 +0300

    update main2

--- main/views.py
+++ main/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 
+from main.models import Student
+
 
 def index(request):
+    student_list = Student.objects.all()
+    context = {
+        'object_list': student_list,
+        'title': 'Главная'
+    }
+
+    return render(request, 'main/index.html', context)
+
+
+def contact(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
         print(f'{name} ({email}): {message}')
 
-    return render(request, 'main/index.html')
+    context = {
+        'title': 'Контакты'
+    }
+
+    return render(request, 'main/contact.html', context)
