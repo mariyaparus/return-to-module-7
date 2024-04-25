94be14c732a90a7f092bcf88dca996a441bae7bb
MariyaParus <cat-she@mail.ru>
2024-04-25 18:52:39 +0300

    update main views.py

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from main.models import Student
+from main.forms import StudentForm, SubjectForm
+from main.models import Student, Subject
 
 
 class StudentListView(ListView):
@@ -25,7 +27,7 @@ class StudentDetailView(DetailView):
 
 class StudentCreateView(CreateView):
     model = Student
-    fields = ('first_name', 'last_name', 'avatar',)
+    form_class = StudentForm
     success_url = reverse_lazy('main:index')
 
     def get_context_data(self, **kwargs):
@@ -36,13 +38,29 @@ class StudentCreateView(CreateView):
 
 class StudentUpdateView(UpdateView):
     model = Student
-    fields = ('first_name', 'last_name', 'avatar',)
+    form_class = StudentForm
     success_url = reverse_lazy('main:index')
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({'title': 'Редактирование студента'})
-        return context
+        context_data = super().get_context_data(**kwargs)
+        SubjectFormset = inlineformset_factory(Student, Subject, form=SubjectForm, extra=1)
+        if self.request.method == 'POST':
+            context_data['formset'] = SubjectFormset(self.request.POST, instance=self.object)
+        else:
+            context_data['formset'] = SubjectFormset(instance=self.object)
+
+        context_data.update({'title': 'Редактирование студента'})
+
+        return context_data
+
+    def form_valid(self, form):
+        formset = self.get_context_data()['formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+
+        return super().form_valid(form)
 
 
 class StudentDeleteView(DeleteView):
