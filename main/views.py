267a28db64258ad42666687b79d326486754941b
MariyaParus <cat-she@mail.ru>
2024-04-25 16:00:58 +0300

    update mail views & urls

--- main/views.py
+++ main/views.py
@@ -1,16 +1,75 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from main.models import Student
 
 
-def index(request):
-    student_list = Student.objects.all()
+class StudentListView(ListView):
+    model = Student
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'title': 'Главная'})
+        return context
+
+
+class StudentDetailView(DetailView):
+    model = Student
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'title': 'Студент'})
+        return context
+
+
+class StudentCreateView(CreateView):
+    model = Student
+    fields = ('first_name', 'last_name', 'avatar',)
+    success_url = reverse_lazy('main:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'title': 'Создание студента'})
+        return context
+
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    fields = ('first_name', 'last_name', 'avatar',)
+    success_url = reverse_lazy('main:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'title': 'Редактирование студента'})
+        return context
+
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    success_url = reverse_lazy('main:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'title': 'Удаление студента'})
+        return context
+
+
+def toggle_activity(request, pk):
+    student_item = get_object_or_404(Student, pk=pk)
+    if student_item.is_active:
+        student_item.is_active = False
+    else:
+        student_item.is_active = True
+
+    student_item.save()
+
     context = {
-        'object_list': student_list,
-        'title': 'Главная'
+        'object': student_item,
+        'title': 'Активен?',
     }
 
-    return render(request, 'main/index.html', context)
+    return redirect(reverse('main:index'), context)
 
 
 def contact(request):
@@ -25,11 +84,3 @@ def contact(request):
     }
 
     return render(request, 'main/contact.html', context)
-
-
-def view_student(request, pk):
-    student_item = get_object_or_404(Student, pk=pk)
-    context = {
-        'object': student_item
-    }
-    return render(request, 'main/student_detail.html', context)
