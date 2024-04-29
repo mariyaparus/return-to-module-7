4e73af45cbfe1d04c069f2c0d03e8c51e68cf442
MariyaParus <cat-she@mail.ru>
2024-04-29 20:48:09 +0300

    update data.json

---
+++ users/management/commands/csu.py
@@ -0,0 +1,10 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email=
+        )
