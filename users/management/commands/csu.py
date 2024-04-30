950deffb1a610dac7581dae44fbb51c3098a8c73
MariyaParus <cat-she@mail.ru>
2024-04-30 13:27:17 +0300

    update users commands csu

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -6,5 +6,12 @@ from users.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
-            email=
+            email='cat-she@mail.ru',
+            first_name='Mariya',
+            last_name='Parus',
+            is_staff=True,
+            is_superuser=True
         )
+
+        user.set_password('12345qwerty')
+        user.save()
