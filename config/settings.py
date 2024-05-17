df6ee4addd0093acfb16f84d9eccd943430ad7d2
MariyaParus <cat-she@mail.ru>
2024-05-17 17:50:03 +0300

    update config settings.py

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,13 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 LOGIN_URL = '/users/'
+
+CACHE_ENABLED = True
+
+if CACHE_ENABLED:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": "redis://127.0.0.1:6379",
+        }
+    }
